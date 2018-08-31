ac66a5162609017eb3784c91e62b73c65a742e69
michaelorchard <42854117+michaelorchard@users.noreply.github.com>
2018-08-30 22:12:55 -0400

    TestMavensHelloWorld

---
+++ testmavenhelloworld/src/main/java/testmavenheroku/testmavenhelloworld/App.java
@@ -0,0 +1,13 @@
+package testmavenheroku.testmavenhelloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
